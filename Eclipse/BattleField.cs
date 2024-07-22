4c4bd32c2475dfcc45570513bd4fa3c1fe141f07
Kim Rune Solstad <krs@itverket.no>
2024-07-22 14:06:17 +0200

    .net8 => .net7

---
+++ Eclipse/BattleField.cs
@@ -0,0 +1,26 @@
+﻿namespace Eclipse;
+
+public class BattleField
+{
+    private IEnumerable<SpaceShip> _attackingFleet;
+    private IEnumerable<SpaceShip> _defendingFleet;
+
+    public BattleField(
+        IEnumerable<SpaceShip> attackingFleet,
+        IEnumerable<SpaceShip> defendingFleet
+    )
+    {
+        _attackingFleet = attackingFleet;
+        _defendingFleet = defendingFleet;
+    }
+
+    public (IEnumerable<SpaceShip> attackingFleet, IEnumerable<SpaceShip> defendingFleet) Commit()
+    {
+        var attackersByInitiative = _attackingFleet.GroupBy(x => x.Initiative);
+        var defendersByInitiative = _defendingFleet.GroupBy(x => x.Initiative);
+        
+        
+        
+        return (_attackingFleet, new List<SpaceShip>());
+    }
+}
